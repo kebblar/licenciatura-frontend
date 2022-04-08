5dd5b416e460ad56dbbb12d60f8b13287631b7a4
mentesniker <mentesniker@gmail.com>
2022-04-07 23:06:45 -0500

    se agregaron los archivos html para el despliegue en github pages

--- src/sites.js
+++ src/sites.js
@@ -1,4 +1,4 @@
-const SERVER = 'http://localhost:8080'
-//const SERVER = 'http://54.163.109.221:56061/';
+//const SERVER = 'http://localhost:8080'
+const SERVER = 'http://54.163.109.221:56061/';
 
 export default SERVER;
